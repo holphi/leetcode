39776add2e45cf95ca4de3fc661802fdb881854f
Alex Li <alex.li@vungle.com>
2021-07-04 17:45:01 +0800

    two_sum.py

--- Easy/two_sum.py
+++ Easy/two_sum.py
@@ -1,4 +1,5 @@
 class Solution:
+
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)-1):
             j = i + 1
@@ -9,6 +10,20 @@ class Solution:
                     j = j + 1
         else:
             return []
+    
+    def twoSum_hash(self, nums: list[int], target: int) -> list[int]:
+        d = {}
+        for i in range(len(nums)):
+            d[nums[i]] = i
+        
+        for i in range(len(nums)):
+            t = target - nums[i]
+            if t in d.keys():
+                return [i ,d[t]]
+        else:
+            return []
 
 s = Solution()
+
 print(s.twoSum([2,8,12,9], 17))
+print(s.twoSum_hash([2,8,12,9], 17))
